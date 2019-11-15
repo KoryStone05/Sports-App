b6913021fb195456510e829b8bb3c5ff837391cb
Dakota Smothers <dsmothers192@gmail.com>
2019-11-15 10:16:09 -0600

    did stuff

--- src/NFL/NFL.js
+++ src/NFL/NFL.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 class NFL extends Component{
+    getStats = () => {
+        axios({
+            method: 'GET',
+            url: 'https://www.thesportsdb.com/api/v1/json/1/searchteams.php'
+        })
+        .then(function(response){
+            console.log(response);
+        })
+        .catch(function(error){
+            console.log(error, 'YOU SUCK');
+        });
+    }
+
     render(){
+        this.getStats();
+
         return(
             <div>
-                <h1>Hi</h1>
+                <h1>NFL</h1>
+
             </div>
         );
     }
