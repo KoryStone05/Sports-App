3033d8a4a73dda0144cdeb360063bfe0928de635
Kory Stone <kory.stone10@gmail.com>
2019-11-17 08:29:30 -0600

    Added the score for the hnome team and the away team on the NBA component, also added styling

---
+++ src/Header/Header.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { textFieldValue: '' }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ textFieldValue: event.target.value })
+    }
+
+    handleChange(event) {
+        this.setState({ textFieldValue: event.target.value })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.getHomeTeamByNameHandler(this.state.textFieldValue);
+        this.setState({ textFieldValue: '' });
+
+    }
+
+
+    render() {
+        return(
+            <div>
+                <div className='nbaHeader'>
+                <h1 className='nba'>NBA</h1>
+                </div>
+                <div>
+                <p className='nbaGames'><u>Today's Games</u></p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
--- src/NBA/NBA.js
+++ src/NBA/NBA.js
@@ -1,38 +1,123 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Header from '../Header/Header';
 
 class NBA extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { team: [] }
+        this.state = { gameData: [] }
     }
 
     componentDidMount() {
-        this.getTeamByName();
+        this.getHomeTeamByName();
     }
 
-    getTeamByName = () => {
+    getHomeTeamByName = (name) => {
         const _this = this;
 
         axios({
             method: 'GET',
-            url: 'https://www.thesportsdb.com/api/v1/json/1/searchteams.php'
+            url: 'https://www.balldontlie.io/api/v1/games',
+            params: {
+                start_date: '2019-11-16',
+                end_date: '2019-11-16',
+                per_page: 15
+            }
         })
         .then(function(response){
-            console.log(response);
+            _this.setState({ gameData: response.data.data || [] });
+            
         })
         .catch(function(error){
             console.log(error);
         });
     }
 
+    showGameData() {
+        
+        console.log(this.state.gameData);
+        if (this.state.gameData.length > 0) {
+            return (
+                <div>
+                    <div>
+                    <p>{this.state.gameData[0].visitor_team.full_name}</p>
+                    <p>{this.state.gameData[0].visitor_team_score}</p>
+                    <p>{this.state.gameData[0].status}</p>
+                    <p>{this.state.gameData[0].home_team_score}</p>
+                    <p>{this.state.gameData[0].home_team.full_name}</p>
+                    </div>
+
+                    <div>
+                    <p>{this.state.gameData[1].visitor_team.full_name}</p>
+                    <p>{this.state.gameData[1].visitor_team_score}</p>
+                    <p>{this.state.gameData[1].status}</p>
+                    <p>{this.state.gameData[1].home_team_score}</p>
+                    <p>{this.state.gameData[1].home_team.full_name}</p>
+                    </div>
+
+                    <div>
+                    <p>{this.state.gameData[2].visitor_team.full_name}</p>
+                    <p>{this.state.gameData[2].visitor_team_score}</p>
+                    <p>{this.state.gameData[2].status}</p>
+                    <p>{this.state.gameData[2].home_team_score}</p>
+                    <p>{this.state.gameData[2].home_team.full_name}</p>
+                    </div>
+
+                    <div>
+                    <p>{this.state.gameData[3].visitor_team.full_name}</p>
+                    <p>{this.state.gameData[3].visitor_team_score}</p>
+                    <p>{this.state.gameData[3].status}</p>
+                    <p>{this.state.gameData[3].home_team_score}</p>
+                    <p>{this.state.gameData[3].home_team.full_name}</p>
+                    </div>
+
+                    <div>
+                    <p>{this.state.gameData[4].visitor_team.full_name}</p>
+                    <p>{this.state.gameData[4].visitor_team_score}</p>
+                    <p>{this.state.gameData[4].status}</p>
+                    <p>{this.state.gameData[4].home_team_score}</p>
+                    <p>{this.state.gameData[4].home_team.full_name}</p>
+                    </div>
+
+                    <div>
+                    <p>{this.state.gameData[5].visitor_team.full_name}</p>
+                    <p>{this.state.gameData[5].visitor_team_score}</p>
+                    <p>{this.state.gameData[5].status}</p>
+                    <p>{this.state.gameData[5].home_team_score}</p>
+                    <p>{this.state.gameData[5].home_team.full_name}</p>
+                    </div>
+
+                    <div>
+                    <p>{this.state.gameData[6].visitor_team.full_name}</p>
+                    <p>{this.state.gameData[6].visitor_team_score}</p>
+                    <p>{this.state.gameData[6].status}</p>
+                    <p>{this.state.gameData[6].home_team_score}</p>
+                    <p>{this.state.gameData[6].home_team.full_name}</p>
+                    </div>
+
+                    <div>
+                    <p>{this.state.gameData[7].visitor_team.full_name}</p>
+                    <p>{this.state.gameData[7].visitor_team_score}</p>
+                    <p>{this.state.gameData[7].status}</p>
+                    <p>{this.state.gameData[7].home_team_score}</p>
+                    <p>{this.state.gameData[7].home_team.full_name}</p>
+                    </div>
+
+                </div>
+                
+            );
+        }
+    }
+
     render() {
-
-        this.getTeamByName();
-
         return(
-            <h1>NBA</h1>
+            <div>
+                <Header 
+                    getHomeTeamByNameHandler={this.getHomeTeamByName}
+                />
+                {this.showGameData()}
+            </div>
         );
     }
 }
