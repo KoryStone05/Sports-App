f629bc5ae4424c9c346c39e0332dfc8a37251834
Kory Stone <kory.stone10@gmail.com>
2019-11-17 18:57:08 -0600

    Added class names for the divs and added styling

--- src/NBA/NBA.js
+++ src/NBA/NBA.js
@@ -20,8 +20,8 @@ class NBA extends Component {
             method: 'GET',
             url: 'https://www.balldontlie.io/api/v1/games',
             params: {
-                start_date: '2019-11-16',
-                end_date: '2019-11-16',
+                start_date: '2019-11-17',
+                end_date: '2019-11-17',
                 per_page: 15
             }
         })
@@ -35,73 +35,70 @@ class NBA extends Component {
     }
 
     showGameData() {
-        
+
+
         console.log(this.state.gameData);
         if (this.state.gameData.length > 0) {
             return (
                 <div>
-                    <div>
-                    <p>{this.state.gameData[0].visitor_team.full_name}</p>
-                    <p>{this.state.gameData[0].visitor_team_score}</p>
-                    <p>{this.state.gameData[0].status}</p>
-                    <p>{this.state.gameData[0].home_team_score}</p>
-                    <p>{this.state.gameData[0].home_team.full_name}</p>
+                    <div className='gameDataContainerOne'>
+                    <p className='visitorTeam'>{this.state.gameData[0].visitor_team.full_name}</p>
+                    <p className='visitorScore'>{this.state.gameData[0].visitor_team_score}</p>
+                    <div className='gameDataStatusContainer'>
+                        <p>{this.state.gameData[0].status}</p>
+                    </div>
+                    <p className='homeScore'>{this.state.gameData[0].home_team_score}</p>
+                    <p className='homeTeam'>{this.state.gameData[0].home_team.full_name}</p>
                     </div>
 
-                    <div>
-                    <p>{this.state.gameData[1].visitor_team.full_name}</p>
-                    <p>{this.state.gameData[1].visitor_team_score}</p>
-                    <p>{this.state.gameData[1].status}</p>
-                    <p>{this.state.gameData[1].home_team_score}</p>
-                    <p>{this.state.gameData[1].home_team.full_name}</p>
+                    <div className='gameDataContainerTwo'>
+                    <p className='visitorTeam'>{this.state.gameData[1].visitor_team.full_name}</p>
+                    <p className='visitorScore'>{this.state.gameData[1].visitor_team_score}</p>
+                    <div className='gameDataStatusContainer'>
+                        <p>{this.state.gameData[1].status}</p>
+                    </div>
+                    <p className='homeScore'>{this.state.gameData[1].home_team_score}</p>
+                    <p className='homeTeam'>{this.state.gameData[1].home_team.full_name}</p>
                     </div>
 
-                    <div>
-                    <p>{this.state.gameData[2].visitor_team.full_name}</p>
-                    <p>{this.state.gameData[2].visitor_team_score}</p>
-                    <p>{this.state.gameData[2].status}</p>
-                    <p>{this.state.gameData[2].home_team_score}</p>
-                    <p>{this.state.gameData[2].home_team.full_name}</p>
+                    <div className='gameDataContainerThree'>
+                    <p className='visitorTeam'>{this.state.gameData[2].visitor_team.full_name}</p>
+                    <p className='visitorScore'>{this.state.gameData[2].visitor_team_score}</p>
+                    <div className='gameDataStatusContainer'>
+                        <p>{this.state.gameData[2].status}</p>
+                    </div>
+                    <p className='homeScore'>{this.state.gameData[2].home_team_score}</p>
+                    <p className='homeTeam'>{this.state.gameData[2].home_team.full_name}</p>
                     </div>
 
-                    <div>
-                    <p>{this.state.gameData[3].visitor_team.full_name}</p>
-                    <p>{this.state.gameData[3].visitor_team_score}</p>
-                    <p>{this.state.gameData[3].status}</p>
-                    <p>{this.state.gameData[3].home_team_score}</p>
-                    <p>{this.state.gameData[3].home_team.full_name}</p>
+                    <div className='gameDataContainerFour'>
+                    <p className='visitorTeam'>{this.state.gameData[3].visitor_team.full_name}</p>
+                    <p className='visitorScore'>{this.state.gameData[3].visitor_team_score}</p>
+                    <div className='gameDataStatusContainer'>
+                        <p>{this.state.gameData[3].status}</p>
+                    </div>
+                    <p className='homeScore'>{this.state.gameData[3].home_team_score}</p>
+                    <p className='homeTeam'>{this.state.gameData[3].home_team.full_name}</p>
                     </div>
 
-                    <div>
-                    <p>{this.state.gameData[4].visitor_team.full_name}</p>
-                    <p>{this.state.gameData[4].visitor_team_score}</p>
-                    <p>{this.state.gameData[4].status}</p>
-                    <p>{this.state.gameData[4].home_team_score}</p>
-                    <p>{this.state.gameData[4].home_team.full_name}</p>
+                    <div className='gameDataContainerFive'>
+                    <p className='visitorTeam'>{this.state.gameData[4].visitor_team.full_name}</p>
+                    <p className='visitorScore'>{this.state.gameData[4].visitor_team_score}</p>
+                    <div className='gameDataStatusContainer'>
+                        <p>{this.state.gameData[4].status}</p>
+                    </div>
+                    <p className='homeScore'>{this.state.gameData[4].home_team_score}</p>
+                    <p className='homeTeam'>{this.state.gameData[4].home_team.full_name}</p>
                     </div>
 
-                    <div>
-                    <p>{this.state.gameData[5].visitor_team.full_name}</p>
-                    <p>{this.state.gameData[5].visitor_team_score}</p>
-                    <p>{this.state.gameData[5].status}</p>
-                    <p>{this.state.gameData[5].home_team_score}</p>
-                    <p>{this.state.gameData[5].home_team.full_name}</p>
+                    <div className='gameDataContainerSix'>
+                    <p className='visitorTeam'>{this.state.gameData[5].visitor_team.full_name}</p>
+                    <p className='visitorScore'>{this.state.gameData[5].visitor_team_score}</p>
+                    <div className='gameDataStatusContainer'>
+                        <p>{this.state.gameData[5].status}</p>
                     </div>
-
-                    <div>
-                    <p>{this.state.gameData[6].visitor_team.full_name}</p>
-                    <p>{this.state.gameData[6].visitor_team_score}</p>
-                    <p>{this.state.gameData[6].status}</p>
-                    <p>{this.state.gameData[6].home_team_score}</p>
-                    <p>{this.state.gameData[6].home_team.full_name}</p>
-                    </div>
-
-                    <div>
-                    <p>{this.state.gameData[7].visitor_team.full_name}</p>
-                    <p>{this.state.gameData[7].visitor_team_score}</p>
-                    <p>{this.state.gameData[7].status}</p>
-                    <p>{this.state.gameData[7].home_team_score}</p>
-                    <p>{this.state.gameData[7].home_team.full_name}</p>
+                    <p className='homeScore'>{this.state.gameData[5].home_team_score}</p>
+                    <p className='homeTeam'>{this.state.gameData[5].home_team.full_name}</p>
                     </div>
 
                 </div>
